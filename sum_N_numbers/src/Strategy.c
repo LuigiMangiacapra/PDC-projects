61bd5494f09135234678a3a9c63ae85cb0f30b60
GiovanniFalcone <giova.falcone@studenti.unina.it>
2023-10-07 20:07:15 +0200

    new file:   sum_N_numbers/src/Strategy.c 	new file:   sum_N_numbers/src/Strategy.h 	modified:   sum_N_numbers/src/sum.c

---
+++ sum_N_numbers/src/Strategy.c
@@ -0,0 +1,118 @@
+/**
+ * @author Fabrizio Vitale
+ * @author Giovanni Falcone
+ * @author Luigi Mangiacapra
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "mpi.h"
+
+/* ******************************************************* */
+/*                      SUPPORT FUNCTION
+/* ******************************************************* */
+
+static void fill_array_randomly(int *elements, int N);
+
+static void fill_array_by_argv(int *elements, int N, char *argv[]);
+
+/* ******************************************************* */
+
+int strategy_is_valid(int strategy){
+    if(strategy < 1 && strategy > 3)
+        return EXIT_FAILURE;
+
+    // it's valid 
+    return 0;
+}
+
+void get_numbers(int *elements, int N, char *argv[]){
+    if(N > 20)
+        fill_array_randomly(elements, N);
+    else
+        fill_array_by_argv(elements, N, argv);
+}
+
+void fill_array_randomly(int *elements, int N){
+    srand(time(NULL));
+
+    printf("Generazione numeri randomici...\n");
+
+    for(int i = 0; i < N; i++){
+        elements[i] = rand() % 100;
+        printf("Element %d: %d\n", i, elements[i]);
+    }
+}
+
+void fill_array_by_argv(int *elements, int N, char *argv[]){
+    printf("Inserimento dei numeri forniti da terminale...\n");
+
+    for(int i = 0; i < N; i++){
+        elements[i] = atoi(argv[i + 3]);    // 0: name src; 1: N; 2: strategy; starting from 3 we have all numbers
+    }
+}
+
+void first_strategy(){
+    if(menum == 0){
+        for(i=1; i<nproc; i++){
+            tag = 80+i;
+            MPI_Recv(&sumparz, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+            sum = sum+sumparz;
+        }
+    }else{
+        tag = menum+80;
+        MPI_Send(&sum, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+    }
+}
+
+void second_strategy(){
+    for(i=0; i<logNproc; i++){
+        int partner = menum ^ (1 << i); // Calcola il processo partner
+
+        if (menum < partner) {
+        int send_tag = 40 + i;
+        int recv_tag = 40 + i;
+
+        // Invia la somma locale al processo partner
+        MPI_Send(&sum, 1, MPI_INT, partner, send_tag, MPI_COMM_WORLD);
+
+        // Ricevi la somma del processo partner
+        MPI_Recv(&sumparz, 1, MPI_INT, partner, recv_tag, MPI_COMM_WORLD, &status);
+
+        // Aggiorna la variabile 'sum' con la somma ricevuta
+        sum += sumparz;
+        } else {
+            int send_tag = 40 + i;
+            int recv_tag = 40 + i;
+
+            // Ricevi la somma dal processo partner
+            MPI_Recv(&sumparz, 1, MPI_INT, partner, recv_tag, MPI_COMM_WORLD, &status);
+
+            // Invia la somma locale al processo partner
+            MPI_Send(&sum, 1, MPI_INT, partner, send_tag, MPI_COMM_WORLD);
+        }
+    }
+}
+
+void third_strategy(){
+    for(i=0; i<logNproc; i++){
+        int partner;
+
+        if ((menum % (int)pow(2, i + 1)) < (1 << i)) {
+            partner = menum + (1 << i);
+            tag = 60 + i;
+
+            // Ricevi da menum + 2^i
+            MPI_Recv(&sumparz, 1, MPI_INT, partner, tag, MPI_COMM_WORLD, &status);
+            sum += sumparz;
+        } else {
+            partner = menum - (1 << i);
+            tag = 60 + i;
+
+            // Invia a menum - 2^i
+            MPI_Send(&sum, 1, MPI_INT, partner, tag, MPI_COMM_WORLD);
+        }
+    }
+}
--- sum_N_numbers/src/sum.c
+++ sum_N_numbers/src/sum.c
@@ -1,147 +1,140 @@
+/**
+ * @author Fabrizio Vitale
+ * @author Giovanni Falcone
+ * @author Luigi Mangiacapra
+*/  
+    
 #include <stdio.h>
-#include <math.h>
-#include <time.h>
 #include <stdlib.h>
-//#include "mpi.h"
+#include <math.h>
+
+#include "mpi.h"
+#include "Strategy.h"
+
+// Support function
+
+/**
+ * @brief check if the inpurs are correct in order to sum
+ * 
+ * @param argc number of parameters
+ * @param N number of elements to sum
+ * @param strategy the strategy to apply
+*/
+static int check_if_inputs_are_valid(int argc, int N, int strategy);
+
+/****************************************************/
 
 int main(int argc, char *argv[]){
     int menum, nproc, tag, i;
     int n, nloc, N, strategia, sum, logNproc, randomNumber;
-    int *x, *xloc;
+    int *x;
+    int *xloc;
     MPI_Status status;
     int sumparz;
 
-    //Parte argv e argc
+    int N;                  // numero di elementi che dovra' contenere l'array
+    int strategy;           // strategia con cui sommare 
+    int *elements;          // array completo
+
+    if(argc < 3){
+        fprintf(stderr, "Utilizzo: <numeri da sommare> <tipo di strategia> <numeri da sommare se N>\n");
+        return EXIT_FAILURE;
+    }
+
+    // convert to integer the number to sum and strategy to apply
     N = atoi(argv[1]);
-    strategia = atoi(argv[2]);
+    strategy = atoi(argv[2]);
 
-    //Controllo input non validi
-    if (N <= 0 || strategia < 1 || strategia > 3) {
-        printf("Valori in input non validi.\n");
-        return 1;
+    if(check_if_inputs_are_valid(argc, N, strategy) != 0)
+        return EXIT_FAILURE;
+
+    // array malloc
+    elements = (int *)malloc(sizeof(int) * N);
+    if(elements == NULL){
+        fprintf(stderr, "Errore nell'allocazione della memoria per l'array 'elements'!\n");
+        return EXIT_FAILURE;
     }
 
-    srand(time(NULL));
+    // fill array
+    get_numbers(elements, N, argv);
+    
+    // show array
+    for(int i = 0; i < N; i++)
+        printf("%d ", elements[i]);
+    printf("\n");
 
-    //Se il numero di elementi è 20 o inferiore riempie l'array con i valori in input, altrimenti con valori randomici
-    if(N <= 20){
-        xloc = (int *)malloc(N * sizeof(int));
-        if (xloc == NULL) {
-            printf("Errore nell'allocazione della memoria per elements.\n");
-            return 1;
-        }
-        for (int i = 0; i < N; i++) {
-            xloc[i] = atoi(argv[i + 3]); // Argv[0] è il nome del programma, quindi iniziamo da argv[3].
-        }
-    }
-    else{
-        xloc = (int *)malloc(N * sizeof(int));
-        if (xloc == NULL) {
-            printf("Errore nell'allocazione della memoria per elements.\n");
-            return 1;
-        }
-        for (int i = 0; i < N; i++) {
-            xloc[i] = rand(); // Genera numeri casuali.
-        }
+    //Seleziona la strategia a seconda se il numero di elementi è potenza di 2, 
+    // se lo e' seleziona la strategia sceltaa se sono la 2 o la 3 altrimenti passa alla 1
+    if((strategy == 2 || strategy == 3) && ((N & (N - 1)) != 0)){
+        strategy = 1;
     }
 
-    //Seleziona la strategia a seconda se il numero di elementi è potenza di 2, se lo è seleziona la strategia sceltaa se sono la 2 o la 3 altrimenti passa alla 1
-    if((strategia == 2 || strategia == 3) && ((N & (N-1)) != 0)){
-        strategia = 1;
+    //per risparmiare tempo computazionale
+    if(strategy == 2 || strategy == 3){
+        logNproc = log2(nproc); 
     }
 
-    if(strategia == 2 || strategia == 3){
-        logNproc = log2(nproc); //per risparmiare tempo computazionale
-    }
-
+    // MPI initialization
     MPI_Init(&argv, &argc);
     MPI_Comm_rank(MPI_COMM_WORLD, &menum);
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
+    // fare funzione esterna
     //il processo 0 invia a tutti gli altri processi il valore con tag 10+i
-    if(menum==0){
+    if(menum == 0){
         //Lettura dei dati di input
-        for(i=1; i<nproc; i++){
-            tag = 10+i;
+        for(i = 1; i < nproc; i++){
+            tag = 10 + i;
             MPI_Send(&n, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
         }
-    }else{//gli altri processi ricevono il valore dal processo 0 con tag 10+numero_processo
-        tag=10+menum;
+    }else{ //gli altri processi ricevono il valore dal processo 0 con tag 10+numero_processo
+        tag = 10 + menum;
         MPI_Recv(&n, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
     }
     
-
-    sum=0;
-    for(i=0; i<N; i++){
-        sum=sum+xloc[i];
+    // xloc, elements, boh 
+    sum = 0;
+    for(i = 0; i < N; i++){
+        sum = sum + xloc[i];
     }
 
-    /*strategia 1*/
-    if(strategia == 1){
-        if(menum == 0){
-            for(i=1; i<nproc; i++){
-                tag = 80+i;
-                MPI_Recv(&sumparz, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
-                sum = sum+sumparz;
-            }
-        }else{
-            tag = menum+80;
-            MPI_Send(&sum, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
-        }
-    }else if(strategia == 2){
-        for(i=0; i<logNproc; i++){
-            int partner = menum ^ (1 << i); // Calcola il processo partner
+    // check the strategy to apply
+    if(strategy == 1)
+        first_strategy();
+    else if(strategy == 2)
+        second_strategy();
+    else
+        third_strategy();
 
-            if (menum < partner) {
-                int send_tag = 40 + i;
-                int recv_tag = 40 + i;
-
-                // Invia la somma locale al processo partner
-                MPI_Send(&sum, 1, MPI_INT, partner, send_tag, MPI_COMM_WORLD);
-
-                // Ricevi la somma del processo partner
-                MPI_Recv(&sumparz, 1, MPI_INT, partner, recv_tag, MPI_COMM_WORLD, &status);
-
-                // Aggiorna la variabile 'sum' con la somma ricevuta
-                sum += sumparz;
-            } else {
-                int send_tag = 40 + i;
-                int recv_tag = 40 + i;
-
-                // Ricevi la somma dal processo partner
-                MPI_Recv(&sumparz, 1, MPI_INT, partner, recv_tag, MPI_COMM_WORLD, &status);
-
-                // Invia la somma locale al processo partner
-                MPI_Send(&sum, 1, MPI_INT, partner, send_tag, MPI_COMM_WORLD);
-            }
-        }
-    }else{
-        for(i=0; i<logNproc; i++){
-            int partner;
-
-            if ((menum % (int)pow(2, i + 1)) < (1 << i)) {
-                partner = menum + (1 << i);
-                tag = 60 + i;
-
-                // Ricevi da menum + 2^i
-                MPI_Recv(&sumparz, 1, MPI_INT, partner, tag, MPI_COMM_WORLD, &status);
-                sum += sumparz;
-            } else {
-                partner = menum - (1 << i);
-                tag = 60 + i;
-
-                // Invia a menum - 2^i
-                MPI_Send(&sum, 1, MPI_INT, partner, tag, MPI_COMM_WORLD);
-            }
-        }
-    }
-
-    if(strategia == 1){
+    // print result
+    if(strategy == 1){
         if(menum == 0)
-            printf("\nSomma totale=%d\n", sum);
+            printf("\nSomma totale = %d\n", sum);
     }else{
-        printf("\nSono il processo %d: Somma totale= %d\n", menum, sum);
+        printf("\nSono il processo %d: e la somma totale= %d\n", menum, sum)
     }
+
+    // freeing memory before program termination 
+    free(elements);
 
     return 0;
+}
+
+static int check_if_inputs_are_valid(int argc, int N, int strategy){
+    if(N <= 20 && argc - 3 != N && strategy_is_valid(strategy) == 0){
+        fprintf(stderr, "Il numero di elementi inserito non corrisponde ad N!\n");
+        return EXIT_FAILURE;
+    }
+
+    if(N <= 0){
+        fprintf(stderr, "Inserire un numero maggiore di 0!\n");
+        return EXIT_FAILURE;
+    }
+
+    if(strategy_is_valid(strategy) != 0){
+        fprintf(stderr, "La strategia deve essere un valore compreso tra 1 e 3!\n");
+        return EXIT_FAILURE;
+    }
+
+    return 0; // valid input
 }
