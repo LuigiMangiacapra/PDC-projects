4d332f55eb6044cbfc6dd2c59f7c080cecb77208
GGGoat98 <92877774+LuigiMangiacapra@users.noreply.github.com>
2023-10-07 12:06:10 +0200

    push sum.c

---
+++ sum.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <math.h>
+#include "mpi.h"
+
+int main(int argc, char *argv[]){
+    int menum, nproc, ...;
+    int n, nloc, tag, i, ...;
+    int N, strategia;
+    int sum;
+    int *x, *xloc;
+    MPI_Status status;
+    int sumparz;
+    int logNproc;
+
+    //Parte argv e argc
+    N = atoi(argv[1]);
+    strategia = atoi(argv[2]);
+
+    if(N <= 20){
+        //prendere in input i numeri fino ad N
+    }else{
+        //Generare N numeri random interi
+    }
+
+    if((strategia == 2 || strategia == 3) && ((N & (N-1)) != 0)){
+        strategia = 1;
+    }else if(strategia > 3){
+        return 0;
+    }
+
+    if(strategia == 2 || strategia == 3){
+        logNproc = log2(nproc); //per risparmiare tempo computazionale
+    }
+
+    MPI_Init(&argv, &argc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &menum);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+
+    if(menum==0){
+        //Lettura dei dati di input
+        for(i=1; i<nproc; i++){
+            tag = 10+i;
+            MPI_Send(&n, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
+        }
+    }else{
+        tag=10+menum;
+        MPI_Recv(&n, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+    }
+    
+    sum=0;
+    for(i=0; i<nloc; i++){
+        sum=sum+xloc[i];
+    }
+
+    /*strategia 1*/
+    if(strategia == 1){
+        if(menum == 0){
+            for(i=1; i<nproc; i++){
+                tag = 80+i;
+                MPI_Recv(&sumparz, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+                sum = sum+sumparz;
+            }
+        }else{
+            tag = menum+80;
+            MPI_Send(&sum, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+        }
+    }else if(strategia == 2){
+        for(i=0; i<logNproc; i++){
+            if((menum % pow(2, i)) == 0){
+                if((menum % pow(2, i+1)) == 0){
+                    tag = 40+i;
+                    //MPI_Recv(&sumparz, 1, MPI_INT, ?, tag, MPI_COMM_WORLD, &status);
+                    //Ricevi da menum+2
+                    sum = sum+sumparz;
+                }else{
+                    tag = 40+i;
+                    //MPI_Send(&sum, 1, MPI_INT, ? , tag, MPI_COMM_WORLD);
+                    //Invia a menum-2
+                }
+            }
+        }
+    }else{
+        for(i=0; i<logNproc; i++){
+            if((menum % pow(2, i+1)) < 2^i){
+                tag = 60+i;
+                //Ricevi da menum+2^i
+                //Invia a menum-2^i
+                sum = sum+sumparz;
+            }else{
+                tag = 60+i;
+                //Ricevi da menum+2^i
+                //Invia a menum-2^i
+                sum = sum+sumparz;
+            }
+        }
+    }
+
+    if(strategia == 1){
+        if(menum == 0)
+            printf("\nSomma totale=%d\n", sum);
+    }else{
+        printf("\nSono il processo %d: Somma totale= %d\n", menum, sum)
+    }
+
+    return 0;
+}
